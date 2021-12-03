b7b49277e33aa7673dd20f6b7818cb186595899f
ureChanger <rlfgus8708@gmail.com>
2021-12-03 19:17:52 +0900

    [Created] Filter 기능 추가 (Tag, Follow, Basic 기준 선택 정렬, 선택 이펙트)

--- app/src/main/java/com/kola/kola_beta/main/data/follow/FollowAdapter.java
+++ app/src/main/java/com/kola/kola_beta/main/data/follow/FollowAdapter.java
@@ -1,5 +1,6 @@
 package com.kola.kola_beta.main.data.follow;
 
+import android.graphics.Typeface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,6 +9,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.kola.kola_beta.R;
@@ -17,9 +19,11 @@ import java.util.ArrayList;
 public class FollowAdapter extends RecyclerView.Adapter<FollowAdapter.CustomViewHolder>{
 
     private ArrayList<FollowData> arrayList;
+    private int isChecked;
 
     public FollowAdapter(ArrayList<FollowData> arrayList) {
         this.arrayList = arrayList;
+        this.isChecked = 99999;
     }
 
     @NonNull
@@ -36,20 +40,29 @@ public class FollowAdapter extends RecyclerView.Adapter<FollowAdapter.CustomView
     public void onBindViewHolder(@NonNull FollowAdapter.CustomViewHolder holder, int position) {
         holder.iv_follow.setImageResource(arrayList.get(position).getImg_follow());
         holder.tv_follow.setText(arrayList.get(position).getName_follow());
-
-
-        holder.layout_follow.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String name = holder.tv_follow.getText().toString();
-                Toast.makeText(v.getContext(), name, Toast.LENGTH_SHORT).show();
-            }
-        });
+        if (isChecked == position){
+            holder.iv_follow.setBorderWidth(5);
+            holder.iv_follow.setBorderColor(ContextCompat
+                    .getColor(holder.itemView.getContext(), R.color.kola_blue));
+            holder.tv_follow.setTextColor(ContextCompat.getColor(holder.itemView.getContext(), R.color.kola_blue));
+            holder.tv_follow.setTypeface(holder.tv_follow.getTypeface(), Typeface.BOLD);
+        }
     }
 
     @Override
     public int getItemCount() {
         return (null != arrayList ? arrayList.size() : 0);
+    }
+
+    // 아이템 클릭 리스너 인터페이스 초기화
+    public interface OnItemClickEventListener{
+        void onItemClick(View view, int position);
+    }
+
+    private FollowAdapter.OnItemClickEventListener listener;
+
+    public void setOnItemClickListener(FollowAdapter.OnItemClickEventListener listener){
+        this.listener = listener;
     }
 
     public class CustomViewHolder extends RecyclerView.ViewHolder {
@@ -63,6 +76,29 @@ public class FollowAdapter extends RecyclerView.Adapter<FollowAdapter.CustomView
             this.layout_follow = itemView.findViewById(R.id.layout_follow);
             this.iv_follow = itemView.findViewById(R.id.iv_follow);
             this.tv_follow = itemView.findViewById(R.id.tv_follow);
+
+            this.layout_follow.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    final int position = getAdapterPosition();
+                    if (isChecked == position){
+                        isChecked = 99999;
+                    }else {
+                        isChecked = position;
+                    }
+                    if(position != RecyclerView.NO_POSITION){
+                        listener.onItemClick(view, position);
+                    }
+                }
+            });
         }
+    }
+
+    public int getIsChecked() {
+        return isChecked;
+    }
+
+    public void setIsChecked(int isChecked) {
+        this.isChecked = isChecked;
     }
 }
--- app/src/main/java/com/kola/kola_beta/main/data/follow/FollowData.java
+++ app/src/main/java/com/kola/kola_beta/main/data/follow/FollowData.java
@@ -1,12 +1,22 @@
 package com.kola.kola_beta.main.data.follow;
 
 public class FollowData {
+    private int id_follow;
     private int img_follow;
     private String name_follow;
 
-    public FollowData(int img_follow, String name_follow) {
+    public FollowData(int id_follow, int img_follow, String name_follow) {
+        this.id_follow = id_follow;
         this.img_follow = img_follow;
         this.name_follow = name_follow;
+    }
+
+    public int getId_follow() {
+        return id_follow;
+    }
+
+    public void setId_follow(int id_follow) {
+        this.id_follow = id_follow;
     }
 
     public int getImg_follow() {
